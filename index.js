2fadfcc8524bba8491401534593b0ac854594cc0
John McEwan <bonzojo@proton.me>
2024-07-04 21:36:48 +0100

    updated images, functionality and styling

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const imgArray = [
-    './testImgs/earth0.png',
-    './testImgs/earth1.png',
-    './testImgs/earth2.png',
-    './testImgs/earth3.png',
-    './testImgs/earth4.png',
-    './testImgs/earth5.png',
-    './testImgs/earth6.png',
-    './testImgs/earth7.png',
-    './testImgs/earth8.png',
-    './testImgs/earth9.png',
-    './testImgs/earth10.png',
-    './testImgs/earth11.png'
+    './newImgs/earth0.png',
+    './newImgs/earth1.png',
+    './newImgs/earth2.png',
+    './newImgs/earth3.png',
+    './newImgs/earth4.png',
+    './newImgs/earth5.png',
+    './newImgs/earth6.png',
+    './newImgs/earth7.png',
+    './newImgs/earth8.png',
+    './newImgs/earth9.png',
+    './newImgs/earth10.png',
+    './newImgs/earth11.png'
 ];
 let index = 0;
 let animPlay = false;
 let intervalId = null;
 
+
 let money = 0;
-
-const saverInput = document.getElementById('saverInput');
-const moneyDisplay = Number(saverInput.value);
-
-
 let numDays = 0;
 let numWeeks = 0;
 let numMonths = 0;
 let numYears = 0;
 
+incrementInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+    }
+});
+
 const toggler = document.getElementById('btnToggle');
 toggler.addEventListener('click', () => {
+    if(!animPlay) {
+        increment = Number(incrementInput.value);
+        document.getElementById('moneyAmount').innerHTML = money;
+    }
 
     animPlay = !animPlay;
     if (animPlay) {
@@ -48,10 +54,12 @@ function imgUpdate() {
     document.getElementById('moneyAmount').innerHTML = money;
     index = (index + 1) % imgArray.length;
     if(index === 11){
-        money+=moneyDisplay; //The average yearly salary for a web developer in the uk is £31,892.
+        money+=increment;
         numDays+=1;
         numWeeks = Math.floor(numDays/7);
         numMonths = Math.floor(numDays / 30);
         numYears = Math.floor(numDays/365);
     }
 }
+//The average yearly salary for a web developer in the uk is £31,892.
+// A Reset button would be good
